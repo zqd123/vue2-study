56d1ab9b831ab11627a012f6a975ef3647933435
zhangqd <954850405@qq.com>
2023-07-21 22:42:40 +0800

    Vnode

--- 第九章/VNode.ts
+++ 第九章/VNode.ts
@@ -20,3 +20,11 @@ interface VNode{
 interface VNodeData{}
 interface Vue{}
 interface VNodeComponentOptions{}
+
+enum VNodeType{
+    TextVNode,//文本节点
+    ElementVNode,// 普通元素节点
+    ComponentVNode,//组件节点
+    EmptyVNode,// 没有内容的注释节点
+    CloneVNode, // 克隆节点，可以是以上任意类型的节点，唯一的区别在于isCloned属性为true
+}
